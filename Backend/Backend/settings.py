bce1a5f1739469de0ac37d272f4e54e9906c33d1
manzoor1 <mk8884484@gmail.com>
2025-06-27 01:08:05 +0530

    update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-ys^o&uuk93(944!10b^z02#xe222#rj+yu*+qiced6@l#2k37w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://employeemasterdata-1.onrender.com",
+]
 ALLOWED_HOSTS = ['.render.com']
 
 
