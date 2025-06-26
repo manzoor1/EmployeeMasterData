f62c6967e2955952c884ccbde1aee37f6339fd07
manzoor1 <mk8884484@gmail.com>
2025-06-27 00:32:09 +0530

    update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ys^o&uuk93(944!10b^z02#xe222#rj+yu*+qiced6@l#2k37w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['.render.com']
